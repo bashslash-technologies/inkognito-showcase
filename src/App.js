f45140d4d1a84bf1f91f7e3d7aa70e9909b1b62a
Domey Benjamin <domeybenjamin1@gmail.com>
2020-05-18 18:29:02 +0000

    Update:added somethings

--- src/App.js
+++ src/App.js
@@ -643,7 +643,7 @@ const App = (props) => {
           </div>
         </div>
       </div>
-      <div className="py-16 bg-gray-50 overflow-hidden lg:py-24">
+      <div className="py-10 bg-gray-50 overflow-hidden lg:py-24">
         <div className="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-screen-xl">
           <svg
             className="hidden lg:block absolute left-full transform -translate-x-1/2 -translate-y-1/4"
@@ -1129,6 +1129,40 @@ const App = (props) => {
           </div>
         </div>
       </div>
+      <div className="bg-white">
+        <div className="max-w-screen-xl mx-auto px-4 py-12 sm:px-6 lg:py-16 lg:px-8">
+          <div
+              className="px-6 py-6 bg-indigo-700 rounded-lg md:py-12 md:px-12 lg:py-16 lg:px-16 xl:flex xl:items-center">
+            <div className="xl:w-0 xl:flex-1">
+              <h2 className="text-2xl leading-8 font-extrabold tracking-tight text-white sm:text-3xl sm:leading-9">
+                Want products news and updates?
+              </h2>
+              <p className="mt-3 max-w-3xl text-lg leading-6 text-indigo-200">
+                Sign up for our newsletter to stay up to date.
+              </p>
+            </div>
+            <div className="mt-8 sm:w-full sm:max-w-md xl:mt-0 xl:ml-8">
+              <form className="sm:flex">
+                <input aria-label="Email address" type="email" required
+                       className="appearance-none w-full px-5 py-3 border border-transparent text-base leading-6 rounded-md text-gray-900 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 transition duration-150 ease-in-out"
+                       placeholder="Enter your email"/>
+                <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0">
+                  <button
+                      className="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-400 focus:outline-none focus:bg-indigo-400 transition duration-150 ease-in-out">
+                    Notify me
+                  </button>
+                </div>
+              </form>
+              <p className="mt-3 text-sm leading-5 text-indigo-200">
+                We care about the protection of your data. Read our
+                <a href="#" className="text-white font-medium underline">
+                  Privacy Policy.
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
 
       <section id={"company"} className="bg-white overflow-hidden">
         <div className="relative max-w-screen-xl mx-auto pt-20 pb-12 px-4 sm:px-6 lg:px-8 lg:py-20">
@@ -1251,6 +1285,7 @@ const App = (props) => {
           </div>
         </div>
       </section>
+
 
       {/*footer*/}
       <div className="bg-gray-900">
